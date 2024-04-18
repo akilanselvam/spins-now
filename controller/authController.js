b7f3d09b2a946de4b4218bf7ec2dbdbf21120930
akilanselvam <selvamakilan1@gmail.com>
2024-04-19 02:25:07 +0530

    Collaboration Page Formatted

--- controller/authController.js
+++ controller/authController.js
@@ -83,6 +83,41 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+exports.retriveUserId = catchAsync(async (req, res, next) => {
+  let token;
+  console.log("retriveUserId");
+  if (req.body.token) {
+    token = req.body.token;
+    console.log(token);
+  } else if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(new AppError("Token missing! Please provide a token.", 401));
+  }
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    const freshUser = await User.findById(decoded.id);
+
+    if (!freshUser) {
+      return next(new AppError("The user belonging to this token does no longer exist.", 401));
+    }
+
+    if (freshUser.changedPasswordAfter(decoded.iat)) {
+      return next(new AppError("User recently changed password! Please log in again.", 401));
+    }
+
+    res.status(200).json({
+      status: "success",
+      userId: freshUser._id
+    });
+  } catch (err) {
+    return next(new AppError("Invalid token! Please log in again.", 401));
+  }
+});
+
 exports.expertProtect = catchAsync(async (req, res, next) => {
   let token;
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
